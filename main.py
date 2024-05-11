f2f18560aa0e15a40dacbac709268e9230712346
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2024-05-11 05:15:13 +0100

    Added local files

---
+++ main.py
@@ -0,0 +1,123 @@
+import pygame
+import numpy as np
+from math import *
+from shapes import *
+
+# WIDTH, HEIGHT = 1540, 1000
+WIDTH, HEIGHT = 800, 600
+WHITE = (255,255,255)
+CYAN = (0,255,255)
+BLACK = (0,0,0)
+
+x_angle, y_angle, z_angle = 0, 0, 0
+scale = 100
+circle_pos = [WIDTH/2, HEIGHT/2]
+
+pygame.display.set_caption("3D Visualization of Shapes in Python")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+shape = Cube()
+points = shape.get_points()
+print(points)
+surfaces = shape.get_surfaces()
+connections = shape.get_connections()
+
+projection_matrix = np.matrix([
+    [1,0,0],
+    [0,1,0]
+])
+
+rotated_points = [
+    [n,n,n] for n in range(len(points))
+]
+
+projected_points = [
+    [n,n] for n in range(len(points))
+]
+
+clock = pygame.time.Clock()
+
+def connect_points(i, j, points):
+    pygame.draw.line(screen, WHITE, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+
+
+# Main flow
+while True:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 4:
+                scale += 5
+            elif event.button == 5:
+                scale -= 5
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        y_angle += 0.01
+    if keys[pygame.K_LEFT]:
+        y_angle -= 0.01
+    if keys[pygame.K_UP]:
+        x_angle += 0.01
+    if keys[pygame.K_DOWN]:
+        x_angle -= 0.01
+    if keys[pygame.K_LSHIFT]:
+        z_angle += 0.01
+    if keys[pygame.K_LCTRL]:
+        z_angle -= 0.01
+
+    rotation_x = np.matrix([
+        [1, 0, 0],
+        [0, cos(x_angle), -sin(x_angle)],
+        [0, sin(x_angle), cos(x_angle)],
+    ])
+
+    rotation_y = np.matrix([
+        [cos(y_angle), 0, sin(y_angle)],
+        [0, 1, 0],
+        [-sin(y_angle), 0, cos(y_angle)],
+    ])
+
+    rotation_z = np.matrix([
+        [cos(z_angle), -sin(z_angle), 0],
+        [sin(z_angle), cos(z_angle), 0],
+        [0, 0, 1]
+    ])
+        
+    screen.fill(BLACK)
+
+    i = 0
+        
+    for point in points:
+        rotated_points[i] = np.dot(rotation_x, point.reshape(3,1))
+        rotated_points[i] = np.dot(rotation_y, rotated_points[i])
+        rotated_points[i] = np.dot(rotation_z, rotated_points[i])
+        projected_2d = np.dot(projection_matrix, rotated_points[i])
+
+        x = int(projected_2d[0][0] * scale) + circle_pos[0]
+        y = int(projected_2d[1][0] * scale) + circle_pos[1]
+        projected_points[i] = [x,y]
+        # pygame.draw.circle(screen, CYAN, (x,y), 5)
+        i += 1
+    
+    # for connection in connections:
+    #     connect_points(connection[0], connection[1], projected_points)
+    
+    for surf in range(1, len(surfaces) + 1):
+        # Get the surface normal
+        surface_normal = np.cross(rotated_points[surfaces[surf - 1][1]].flatten() - rotated_points[surfaces[surf - 1][0]].flatten(),
+                                  rotated_points[surfaces[surf - 1][2]].flatten() - rotated_points[surfaces[surf - 1][0]].flatten())
+        
+        # Draw the surface with adjusted color
+        if (surface_normal[0][2] > 0):
+            light_intensity = 130 + -surface_normal[0][1] * 25
+            surface_color = (light_intensity, light_intensity, light_intensity)
+            pygame.draw.polygon(screen, surface_color, [projected_points[i] for i in surfaces[surf - 1]])
+
+    pygame.display.update()
---
+++ shapes.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+PHI = (1 + 5 ** 0.5) / 2
+
+class Polyhedron:
+    def __init__(self):
+        self.points = []
+        self.surfaces = []
+        self.connections = []
+
+    def get_points(self):
+        return self.points
+    
+    def get_connections(self):
+        return self.connections
+    
+    def get_surfaces(self):
+        return self.surfaces
+
+class Cube(Polyhedron):
+    points, connections = [], []
+
+    def __init__(self):
+        self.points.append(np.matrix([-1, -1, 1]))
+        self.points.append(np.matrix([1, -1, 1]))
+        self.points.append(np.matrix([1,  1, 1]))
+        self.points.append(np.matrix([-1, 1, 1]))
+        self.points.append(np.matrix([-1, -1, -1]))
+        self.points.append(np.matrix([1, -1, -1]))
+        self.points.append(np.matrix([1, 1, -1]))
+        self.points.append(np.matrix([-1, 1, -1]))
+
+        self.surfaces = [
+            [0, 1, 2, 3],  # Front
+            [5, 4, 7, 6],  # Back
+            [4, 0, 3, 7],  # Left
+            [1, 5, 6, 2],  # Right
+            [1, 0, 4, 5],  # Bottom
+            [6, 7, 3, 2]   # Top
+        ]
+
+        for p in range(4):
+            self.connections.append([p, (p+1) % 4])
+            self.connections.append([p+4, ((p+1) % 4) + 4])
+            self.connections.append([p, (p+4)])
+
+class Pyramid(Polyhedron):
+    points, connections = [], []
+
+    def __init__(self):
+        self.points.append(np.matrix([0, -1, 0]))
+        self.points.append(np.matrix([-1, 1, 1]))
+        self.points.append(np.matrix([1, 1, 1]))
+        self.points.append(np.matrix([-1, 1, -1]))
+        self.points.append(np.matrix([1, 1, -1]))
+
+        self.surfaces = [
+            [2, 1, 0],
+            [4, 2, 0],
+            [3, 4, 0],
+            [1, 3, 0],
+            [3, 1, 2, 4],
+        ]
+
+        for i in range(1,5):
+            self.connections.append([0,i])
+        for i in range(1,3):
+            self.connections.append([(i*3)-2, 2])
+            self.connections.append([(i*3)-2, 3])
+
+class Icosahedron(Polyhedron):
+    points, connections, surfaces = [], [], []
+
+    def __init__(self):
+        vertices = [
+            [PHI, 1, 0], [PHI, -1, 0], [-PHI, 1, 0], [-PHI, -1, 0],
+            [0, PHI, 1], [0, PHI, -1], [0, -PHI, 1], [0, -PHI, -1],
+            [1, 0, PHI], [-1, 0, PHI], [1, 0, -PHI], [-1, 0, -PHI]
+        ]
+        
+        for vertex in vertices:
+            self.points.append(np.matrix(vertex))
+
+        for i in range(len(vertices)):
+            for j in range(i + 1, len(vertices)):
+                distance = np.linalg.norm(np.array(vertices[i]) - np.array(vertices[j]))
+                if abs(distance) == 2.0:
+                    self.connections.append([i, j])
+
+        self.generate_surfaces()
+    
+    def generate_surfaces(self):
+        # Define the indices of vertices forming each triangle surface
+        self.surfaces = [
+            [0, 1, 4], [0, 4, 8], [0, 8, 9], [0, 9, 2], [0, 2, 1],
+            [3, 2, 9], [3, 9, 11], [3, 11, 7], [3, 7, 5], [3, 5, 2],
+            [6, 1, 2], [6, 2, 5], [6, 5, 10], [6, 10, 4], [6, 4, 1],
+            [7, 8, 4], [7, 4, 10], [7, 10, 11], [7, 11, 9], [7, 9, 8],
+            [10, 5, 7], [10, 7, 11], [10, 11, 9], [10, 9, 8], [10, 8, 4]
+        ]
+        return self.surfaces
